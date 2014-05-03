362d3f9c5031548a690f34b6f81890ed4a1a314b
aryner <aryner@gmail.com>
2014-05-02 21:12:00 -0700

    got > in myshell sort of working, right now it prepends instead of overwriting

--- 415/hw7/myShell.c
+++ 415/hw7/myShell.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
+#include <fcntl.h>
 #include <sys/types.h>
 #include <string.h>
 #include <unistd.h>
 #include <sys/wait.h>
+
+int command(char* input, pid_t pid, int status, char *arg, char **argv, int count);
+int rightArrow(char* right, char* input, pid_t pid, int status, char *arg, char **argv, int count);
 
 int main() {
   pid_t pid;
@@ -14,7 +18,7 @@ int main() {
   char *pos;
 
   while(1) {
-    printf("myShell> ");
+    printf("myShell>> ");
     fgets(input, 4096, stdin);
     if((pos = strchr(input, '\n')) != NULL) {
       *pos = '\0';
@@ -23,31 +27,82 @@ int main() {
     if(!strncmp(input,"exit",4)){
       break;
     }
- 
-    if ((pid = fork()) < 0) {
-      fprintf(stderr, "error forking child process\n");
+    
+    if(command(input,pid,status,arg,argv,count) < 0) {
       break;
-    }
-
-    if (pid == 0) {
-      arg = strtok(input, " ");
-      count = 0;
-      while(arg != NULL) {
-        argv[count] = arg;
-        arg = strtok(NULL, " ");
-        count++;
-      }
-      argv[count] = NULL;
-
-      if(execvp(argv[0], argv) < 0) {
-        fprintf(stderr, "error: Invalid command\n");
-        break;
-      }
-    }
-    else {
-      while(wait(&status) != pid);
     }
   }
 
   return 0;
 }
+
+int rightArrow(char* right, char* input, pid_t pid, int status, char *arg, char **argv, int count){
+  int file = open((right+2), O_CREAT|O_WRONLY);
+  
+  if((pid = fork()) < 0) {
+    fprintf(stderr, "error forking child process\n");
+    close(file);
+    return -1;
+  }
+  if(pid == 0) {
+    arg = strtok(input, " ");
+    count = 0;
+    while(arg < right) {
+      argv[count] = arg;
+      arg = strtok(NULL, " ");
+      count++;
+    }
+    argv[count] = NULL;
+    close(1);
+    close(2);
+    dup(file);
+    dup(file);
+
+    if(execvp(argv[0], argv) < 0) {
+      fprintf(stderr, "error: Invalid command\n");
+      close(file);
+      return -1;
+    }
+  }
+  else {
+    while(wait(&status) != pid);
+  } 
+
+  close(file);
+  return 0;
+}
+
+int command(char* input, pid_t pid, int status, char *arg, char **argv, int count){
+  char* right;
+
+  if((right = strstr(input, ">"))) {
+    return rightArrow(right,input,pid,status,arg,argv,count);
+  }
+
+  if ((pid = fork()) < 0) {
+    fprintf(stderr, "error forking child process\n");
+    return -1;
+  }
+
+  if (pid == 0) {
+    arg = strtok(input, " ");
+    count = 0;
+    while(arg != NULL) {
+      argv[count] = arg;
+      arg = strtok(NULL, " ");
+      count++;
+    }
+    argv[count] = NULL;
+
+    if(execvp(argv[0], argv) < 0) {
+      fprintf(stderr, "error: Invalid command\n");
+      return -1;
+    }
+  }
+  else {
+    while(wait(&status) != pid);
+  }
+
+  return 0;
+}
+
